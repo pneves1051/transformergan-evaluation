a6409f106eb6356775e0a0400f3fb63a204e8889
pneves1051 <pedro_ltn@outlook.com>
2021-12-11 15:32:22 -0300

    Insert required fields.

--- tests_1/main.js
+++ tests_1/main.js
@@ -8,19 +8,14 @@ const startBtn = document.querySelectorAll('form .botao-inic');
 const form = document.querySelectorAll('form');
 
 const audios = document.querySelectorAll('audio');
-console.log(audios);
 
 
 /* CHANGE ID FOR EACH USER*/
 var loc = window.location.pathname;
-console.log(loc)
 last_bar = loc.lastIndexOf('/')
 /* Get only outermost parent folder */
 var dir = loc.substring(loc.slice(0, last_bar).lastIndexOf('/')+1, last_bar)
 var id = dir.replace('tests', '')
-console.log(dir)
-console.log(id)
-
 
 let root_list =  ['./samples' + id + '/generated_cond_tr_checkpoint_audio/generated_',
                   './samples' + id + '/generated_cond_tr_checkpoint_pretrain_rnn_audio/generated_',
@@ -34,9 +29,8 @@ for (let i = 0; i < audios.length; i++) {
     } else {
         root = root_list[2];
     }
-    
+   
     new_src =  root + (i%4).toString() + id + '.mp3' 
-    console.log(new_src)
     audios[i].setAttribute('src', new_src)
 }
 
@@ -71,7 +65,6 @@ const shuffleArray = array => {
     }
 }
 shuffleArray(array);
-console.log(array);
 
 /* Turn last button into submit */
 lastBtn = nextBtn[array[nextBtn.length-1]];
@@ -93,36 +86,68 @@ function changeStep(btn){
     const active = document.querySelector('form .step.active');
     /*Position of the active fieldset on the list of fieldsets*/
     index = steps.indexOf(active);
-    /*Remove active tag from fieldset*/
-    steps[index].classList.remove('active');
-    /*Position of the previously
-      active fieldset on the shuffled array*/
-    array_index = array.indexOf(index);
-    /* Stop audio if it is curently being played*/
-    if (!audios[array[array_index]].paused){
-        audios[array[array_index]].pause();
+    
+    /* Check if fields are all valid (only if wanting to to to next step)*/
+    var isvalid = true;
+    if (btn == 'next') {
+        isvalid = validateForm(index)
     }
+    if (isvalid == true) {
+        /*Remove active tag from fieldset*/
+        steps[index].classList.remove('active');
+        /*Position of the previously
+        active fieldset on the shuffled array*/
+        array_index = array.indexOf(index);
+        /* Stop audio if it is curently being played*/
+        if (!audios[index].paused){
+        audios[index].pause();
+        }
+        if(btn == 'next'){
+            array_index ++;
+        }else if(btn == 'prev'){
+            array_index --;
+        }
+        steps[array[array_index]].classList.add('active');
 
-
-    console.log(index);
-    if(btn == 'next'){
-        array_index ++;
-    }else if(btn == 'prev'){
-        array_index --;
+        window.scrollTo(0, 0);
     }
-    steps[array[array_index]].classList.add('active');
-
-    window.scrollTo(0, 0)
 }
 
 function changeStartStep(){
     /*Remove active tag from fieldset*/
     startStep[0].classList.remove('active');
     /*Change to first step*/
-    console.log(startStep);
     steps[array[0]].classList.add('active');
 }
 
+let features = ['humanity', 'originality', 'quality', 'structure', 'arousal', 'valence']
+function validateForm(idx) {
+    /* For each feature in each sample*/
+    for (i = 0; i < features.length; i++) {
+        /* Get all options from radio button */
+        const radio_name = "sample" + (idx+1).toString() + "_" + features[i];
+        test_concat = `input[name="${radio_name}"]`;
+
+        var radios  = document.querySelectorAll(test_concat)
+        /* Check whether some of the options for that
+            feature and that sample is checked */
+        let isvalid = false;
+        for (j = 0; j < radios.length; j++) {
+            if (radios[j].checked){
+                isvalid = true; 
+                break;
+            }
+        }
+        /* if none of the options are cheked, alert user */
+        if (isvalid == false) {
+            alert("Por favor, preencha todos os campos.")
+            return isvalid
+        }
+    }
+    /* if the function got to this point, isvalid is true*/
+    var isvalid = true;
+    return isvalid
+}
 
 
 /*
